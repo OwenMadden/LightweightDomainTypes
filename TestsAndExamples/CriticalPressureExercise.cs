14e258fae15f930f3a16f562143ac98ab2f136bc
owen.madden <owen.madden@atmosi.com>
2023-02-01 13:45:08 +0000

    Update exercise to contain units

--- TestsAndExamples/CriticalPressureExercise.cs
+++ TestsAndExamples/CriticalPressureExercise.cs
@@ -5,6 +5,6 @@ namespace TestsAndExamples;
 [TestFixture]
 public class CriticalPressureTests
 {
-    // A valid critical pressure should be in the range 10132.57 and 20265100.0
+    // A valid critical pressure should be in the range 10132.57 and 20265100.0 Pa
     // Create a type that encodes that information
 }
