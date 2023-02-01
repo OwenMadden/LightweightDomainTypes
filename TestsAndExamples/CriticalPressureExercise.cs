05d3c89b1bac713116db476777c9eedec34b61e1
owen.madden <owen.madden@atmosi.com>
2023-02-01 10:59:28 +0000

    Added exercise

---
+++ TestsAndExamples/CriticalPressureExercise.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace TestsAndExamples;
+
+[TestFixture]
+public class CriticalPressureTests
+{
+    // A valid critical pressure should be in the range 10132.57 and 20265100.0
+    // Create a type that encodes that information
+}
